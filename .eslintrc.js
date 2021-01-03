469aa660d8aa83814ec166a71cb5ceed42d7dfe4
Jeremias Alvarez <jerealvarez34@gmail.com>
2021-01-03 17:08:57 -0300

    fix linter & prettier, add custom scss

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,5 +16,27 @@ module.exports = {
   ],
   plugins: ['prettier'],
   // add your custom rules here
-  rules: {},
+  rules: {
+    'no-console': 1,
+    'prettier/prettier': 1,
+  },
 }
+
+// {
+//   "root": true,
+//   "parser": "@typescript-eslint/parser",
+//   "plugins": [
+//     "@typescript-eslint",
+//     "prettier"
+//   ],
+//   "extends": [
+//     "eslint:recommended",
+//     "plugin:@typescript-eslint/eslint-recommended",
+//     "plugin:@typescript-eslint/recommended",
+//     "prettier"
+//   ],
+//   "rules": {
+//     "no-console": 1,       // Means warning
+//     "prettier/prettier": 2 // Means error
+//   }
+// }
