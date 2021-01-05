3baa24572df27b233397f6eb092f99f542e18e1d
Jeremias Alvarez <jerealvarez34@gmail.com>
2021-01-05 14:38:13 -0300

    navbar - falta terminar

---
+++ components/Navbar.vue
@@ -0,0 +1,62 @@
+<template>
+	<b-navbar fixed-top="true" spaced="true" type="is-info">
+		<template slot="brand">
+			<b-navbar-item
+				class="link-custom"
+				tag="router-link"
+				:to="{ path: '/' }"
+			>
+				<h3 class="navbar-title">
+					Jeremias <br />
+					Alvarez
+				</h3>
+			</b-navbar-item>
+		</template>
+
+		<template slot="end">
+			<b-navbar-item class="nav-link" href="#"> Inicio </b-navbar-item>
+			<b-navbar-item class="nav-link" href="#info">
+				¿Quien Soy?
+			</b-navbar-item>
+			<b-navbar-item class="nav-link" href="#projects">
+				Proyectos
+			</b-navbar-item>
+			<b-navbar-item class="nav-link" href="#contact">
+				Contacto
+			</b-navbar-item>
+			<!-- <b-navbar-dropdown hoverable="true" label="Info">
+				<b-navbar-item href="#"> About </b-navbar-item>
+				<b-navbar-item href="#"> Contact </b-navbar-item>
+			</b-navbar-dropdown> -->
+		</template>
+	</b-navbar>
+</template>
+
+<style lang="scss" scoped>
+	@import url('https://fonts.googleapis.com/css2?family=Oswald:wght@500;600;700&display=swap');
+
+	$text-hover: rgb(233, 233, 233);
+
+	.link-custom {
+		padding: 0;
+
+		.navbar-title {
+			font-family: 'Oswald', sans-serif;
+			font-size: 2rem;
+			padding: 0;
+			line-height: 40px;
+		}
+	}
+
+	.link-custom:hover {
+		background-color: unset !important;
+
+		.navbar-title {
+			color: $text-hover;
+		}
+	}
+
+	.nav-link {
+		font-size: 1.2rem;
+	}
+</style>
---
+++ pages/test.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<h1>HOLA TEST</h1>
+	</div>
+</template>
+
+<script>
+	export default {};
+</script>
+
+<style scoped lang="scss">
+	div {
+		background-color: red;
+
+		h1 {
+			color: blue;
+		}
+	}
+</style>
