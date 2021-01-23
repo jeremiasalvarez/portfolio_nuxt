10387cd6a8b299d15c67f3c320f6e6e5b4050c2d
Jeremias Alvarez <jerealvarez34@gmail.com>
2021-01-23 16:42:33 -0300

    tab de skills al 50%

---
+++ components/Skills.vue
@@ -0,0 +1,35 @@
+<template>
+	<div>
+		<div
+			v-for="({ name }, index) in skillsArray"
+			:key="index"
+			class="column is-one-quarter"
+		>
+			<div
+				class="columns is-centered is-vcentered is-variable is-1 is-justify-content-flex-start"
+			>
+				<div class="column is-narrow">
+					<b-icon
+						pack="fas"
+						icon="arrow-circle-right"
+						size="is-small"
+						type="is-info"
+					>
+					</b-icon>
+				</div>
+				<div class="column is-narrow">
+					<p>{{ name }}</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: ['skillsArray']
+	};
+</script>
+
+<style>
+</style>
--- components/SkillsTab.vue
+++ components/SkillsTab.vue
@@ -1,6 +1,85 @@
 <template>
-	<div></div>
+	<div class="container has-text-centered mt-5">
+		<div class="columns is-desktop is-centered is-variable is-1">
+			<div class="column">
+				<div
+					class="columns is-vcentered is-desktop is-centered is-variable is-1"
+				>
+					<div class="column is-narrow">
+						<h2 class="title has-text-info">Backend</h2>
+					</div>
+					<div class="column is-narrow">
+						<b-icon
+							pack="fas"
+							icon="server"
+							size="is-medium"
+							type="is-info"
+						>
+						</b-icon>
+					</div>
+				</div>
+				<div class="columns is-centered">
+					<Skills :skillsArray="skillsBackEnd" />
+				</div>
+			</div>
+			<div class="column">
+				<div
+					class="columns is-vcentered is-desktop is-centered is-variable is-1"
+				>
+					<div class="column is-narrow">
+						<h2 class="title has-text-info">Frontend</h2>
+					</div>
+					<div class="column is-narrow">
+						<b-icon
+							pack="fas"
+							icon="laptop"
+							size="is-medium"
+							type="is-info"
+						>
+						</b-icon>
+					</div>
+				</div>
+				<div class="columns is-centered">
+					<Skills :skillsArray="skillsFrontEnd" />
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
+	export default {
+		data() {
+			return {
+				skillsBackEnd: [
+					{
+						name: 'Node JS (Express JS, Sails JS)'
+					},
+					{
+						name: 'ASP.NET Core (MVC - Web API)'
+					},
+					{
+						name: 'PHP'
+					},
+					{
+						name: 'MySQL'
+					}
+				],
+				skillsFrontEnd: [
+					{
+						name: 'Vue JS, Nuxt JS'
+					},
+					{
+						name: 'React JS'
+					},
+					{
+						name: 'Bootstrap'
+					},
+					{
+						name: 'Bulma (Buefy)'
+					}
+				]
+			};
+		}
+	};
 </script>
